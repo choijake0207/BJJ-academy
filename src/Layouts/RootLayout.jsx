f6e9f81c1dc4fdaf5428820c9a594d71dc4825de
choijake0207 <jake.ch0712@gmail.com>
2024-07-18 16:27:03 -0700

    create rootlayout UI: side nav bar and main

---
+++ src/Layouts/RootLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {NavLink, Outlet} from "react-router-dom"
+
+export default function RootLayout() {
+  return (
+    <div className="root-layout">
+        <nav className="root-nav">
+            <NavLink to="/dashboard">Dashboard</NavLink>
+            <NavLink to="/classes">Classes</NavLink>
+            <NavLink to="/account">Account</NavLink>
+            <button>Log Out</button>
+        </nav>
+        <main className="root-main"><Outlet/></main>
+       
+    </div>
+  )
+}
