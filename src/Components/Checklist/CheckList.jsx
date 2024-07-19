43d61d0112ad06315aee8b1c08cf7600b3ff913b
choijake0207 <jake.ch0712@gmail.com>
2024-07-19 09:46:11 -0700

    import form and add toggle form visibility function

--- src/Components/Checklist/CheckList.jsx
+++ src/Components/Checklist/CheckList.jsx
@@ -1,23 +1,28 @@
 import React, { useState } from 'react'
-
-export function CheckListForm () {
-    return (
-        <form className="checklist-form">
-            <textarea
-            placeholder="Add A Training Goal"
-            />
-
-        </form>
-        
-    )
-}
-
+import { CheckListForm } from './CheckListForm'
 
 export default function CheckList() {
     const [list, setList] = useState([])
+    const [formOn, setFormOn] = useState(false)
+    const handleAddGoal = (goal) => {
+        setList(prev => [...prev, goal])
+    }
   return (
-    <div className="checklist">
+    <div className="checklist-component">
         <h4>Training Goals</h4>
+        <ul className="checklist">
+            {list.map(goal => {
+                return (
+                    <p>{goal.text}</p>
+                )
+            })}
+        </ul>
+        <button onClick={() => setFormOn(true)}>Add Goal</button>
+        {formOn  && 
+            <CheckListForm
+                addGoal={handleAddGoal}
+            />
+        }   
 
         
     </div>
