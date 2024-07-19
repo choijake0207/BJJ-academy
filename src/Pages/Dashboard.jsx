9598f6b2131aa16d085120b21b282de4a1db999e
choijake0207 <jake.ch0712@gmail.com>
2024-07-19 08:04:32 -0700

    wrap in PageLayout

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import PageLayout from '../Layouts/PageLayout'
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <PageLayout pageType={"Dashboard"}>
+      <div>Dashboard</div>
+
+    </PageLayout>
+    
   )
 }
