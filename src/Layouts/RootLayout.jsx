3a3780c7610ae0638dde08753c333094bd97c41f
choijake0207 <jake.ch0712@gmail.com>
2024-07-18 18:11:47 -0700

    conditional render login page and rest of rootlayout based on authUser status

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {NavLink, Outlet} from "react-router-dom"
-
+import { AuthContext } from '../Context/AuthContext'
+import Login from '../Pages/Login'
 export default function RootLayout() {
+  const {authUser} = useContext(AuthContext)
   return (
     <div className="root-layout">
-        <nav className="root-nav">
+      {authUser ? 
+        <>
+          <nav className="root-nav">
             <h1>BJJ MGMT</h1>
             <NavLink to="/">Dashboard</NavLink>
             <NavLink to="/classes">Classes</NavLink>
             <NavLink to="/account">Account</NavLink>
             <button>Log Out</button>
-        </nav>
-        <main className="root-main"><Outlet/></main>
+          </nav>
+          <main className="root-main"><Outlet/></main>
+        </> : <Login/>}
+       
        
     </div>
   )
