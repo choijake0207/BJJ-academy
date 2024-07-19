c6dd537df56f326b8aec73fa1420b823ab776696
choijake0207 <jake.ch0712@gmail.com>
2024-07-18 17:09:17 -0700

    create dummy accounts

---
+++ src/Assets/Users.js
@@ -0,0 +1,22 @@
+export const users = [
+    {
+        id: 0,
+        firstName: "John",
+        lastName: "Doe",
+        username: "johndoe",
+        password: "johndoe",
+        todos: [],
+        classes: [],
+        role: "member"
+    },
+    {
+        id: 1,
+        firstName: "Jane",
+        lastName: "Doe",
+        username: "janedoe",
+        password: "janedoe",
+        todos: [],
+        classes: [],
+        role: "admin"
+    }
+]
