ef52216e127604017cb9a06555e214a1c18a275d
choijake0207 <jake.ch0712@gmail.com>
2024-07-18 17:28:39 -0700

    create protected routes and redirect to login if no authUser

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider,Outlet } from 'react-router-dom'
 import RootLayout from './Layouts/RootLayout'
 import Dashboard from './Pages/Dashboard'
 import Classes from './Pages/Classes'
 import Account from './Pages/Account'
-import { AuthContextProvider } from './Context/AuthContext'
+import { AuthContext, AuthContextProvider } from './Context/AuthContext'
+import Login from './Pages/Login'
+
+const ProtectedRoute = () => {
+  const {authUser} = useContext(AuthContext)
+  return authUser ? <Outlet/> : <Navigate to="/login"/>
+}
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout/>}>
-      <Route index element={<Dashboard/>}/>
-      <Route path="/classes" element={<Classes/>}/>
-      <Route path="/account" element={<Account/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route element={<ProtectedRoute/>}>
+        <Route index element={<Dashboard/>}/>
+        <Route path="/classes" element={<Classes/>}/>
+        <Route path="/account" element={<Account/>}/>
+      </Route>
     </Route>
   )
 )
