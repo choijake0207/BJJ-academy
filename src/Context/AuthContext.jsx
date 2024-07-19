3efaf97ff22d63bd0fe08f8d7cb47a318860995e
choijake0207 <jake.ch0712@gmail.com>
2024-07-18 17:39:43 -0700

    add debug loggers

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -14,6 +14,7 @@ export function AuthContextProvider ({children}) {
             alert("Incorrect Credentials")
         } else {
             setAuthUser(user)
+            console.log("Login Success")
         }
     }
 
