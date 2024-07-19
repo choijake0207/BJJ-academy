44d9f6d0e16dc4a57a8db7e5d55a7e972875df99
choijake0207 <jake.ch0712@gmail.com>
2024-07-19 14:56:45 -0700

    add checkbox/delete buttons and event handler prop

--- src/Components/Checklist/ListItem.jsx
+++ src/Components/Checklist/ListItem.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-export default function ListItem({goal}) {
+export default function ListItem({goal, handleCheck, handleDelete}) {
   return (
     <li className="list-item">
+        <input
+          type="checkbox"
+          value={goal.completed}
+          checked={goal.completed === true}
+          onChange={(e) => handleCheck(e.target.checked)}
+        />
         <h4>{goal.text}</h4>
         <p><i>{goal.category}</i></p>
+        <button onClick={() => handleDelete(goal.id)}>Delete</button>
     </li>
   )
 }
