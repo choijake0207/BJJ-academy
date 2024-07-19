becf08e411d47b7f029cecae448c8d03ca239c33
choijake0207 <jake.ch0712@gmail.com>
2024-07-19 10:14:54 -0700

    import styles

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import PageLayout from '../Layouts/PageLayout'
+import CheckList from '../Components/Checklist/CheckList'
+import "../Styles/Dashboard.css"
 
 export default function Dashboard() {
   return (
     <PageLayout pageType={"Dashboard"}>
-      <div>Dashboard</div>
-
+      <div className="dashboard">
+        <CheckList/>
+      </div>
     </PageLayout>
     
   )
