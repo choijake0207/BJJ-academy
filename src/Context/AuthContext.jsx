49a743bee0600df4f916564fb35d6a6440d4e9fe
choijake0207 <jake.ch0712@gmail.com>
2024-07-18 17:19:13 -0700

    create auth context, context provider, and login function w validation logic

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,4 +1,26 @@
-import React, {createContext} from 'react'
+import React, {createContext, useState} from 'react'
+import { Users } from '../Assets/Users'
 
 export const AuthContext = createContext(null)
 
+export function AuthContextProvider ({children}) {
+
+    const [authUser, setAuthUser] = useState()
+
+    const login = ({username, password}) => {
+        const user = Users.find(user => user.username === username && user.password === password)
+        if (!user) {
+            console.log("Incorrect Credentials")
+            alert("Incorrect Credentials")
+        } else {
+            setAuthUser(user)
+        }
+    }
+
+    const values = {authUser, login}
+
+
+    return (
+        <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
+    )
+}
