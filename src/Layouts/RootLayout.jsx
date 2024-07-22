870f817c9e054d10721f8e599187f61e59084197
choijake0207 <jake.ch0712@gmail.com>
2024-07-22 10:22:05 -0700

    refactor to include header

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -9,18 +9,23 @@ export default function RootLayout() {
     <>
       {authUser ? 
         <div className="root-layout">
-          <nav className="root-nav">
-            <div className="nav-header">
-              <img src="src/Assets/bjj_belts_logo_icon (1).svg"/>
-              <h1>BJJ MGMT</h1>
-            </div>
-            
-            <NavLink to="/">Dashboard</NavLink>
-            <NavLink to="/classes">Classes</NavLink>
-            <NavLink to="/account">Account</NavLink>
-            <button onClick={logout}>Log Out</button>
-          </nav>
-          <main className="root-main"><Outlet/></main>
+          <header className="header">
+            <img src="src/Assets/bjj_belts_logo_icon (1).svg"/>
+            <h1>BJJ MGMT</h1>
+          </header>
+          <div className="content-columns">
+            <nav className="side-nav-bar">
+              <div className="nav-header">
+                <h3>Welcome {authUser.firstName}</h3>
+              </div>
+              
+              <NavLink to="/">Dashboard</NavLink>
+              <NavLink to="/classes">Classes</NavLink>
+              <NavLink to="/account">Account</NavLink>
+              <button onClick={logout}>Log Out</button>
+            </nav>
+            <main className="root-main"><Outlet/></main>
+          </div>
         </div> : 
         <Login/>
       } 
