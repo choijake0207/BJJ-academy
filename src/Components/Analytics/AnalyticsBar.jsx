bf3cb2a5258551ddb7d06e6e391b139fed9249ad
choijake0207 <jake.ch0712@gmail.com>
2024-07-22 11:37:03 -0700

    import in reusable widget components and pass props

--- src/Components/Analytics/AnalyticsBar.jsx
+++ src/Components/Analytics/AnalyticsBar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import AnalyticsWidget from './AnalyticsWidget'
 import "../../Styles/Analytics.css"
 
 export default function AnalyticsBar() {
   return (
-    <div className="analytics-bar">AnalyticsWidget</div>
+    <div className="analytics-bar">
+      <AnalyticsWidget widgetType={"Monthly Sales"}/>
+      <AnalyticsWidget widgetType={"Members"}/>
+      <AnalyticsWidget widgetType={"Empty Widget"}/>
+    </div>
   )
 }
