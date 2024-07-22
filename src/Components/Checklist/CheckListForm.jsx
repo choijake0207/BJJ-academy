94899f1b6635c0fc0072f5c32a3cd967a704dd77
choijake0207 <jake.ch0712@gmail.com>
2024-07-22 11:10:06 -0700

    conditional render different forms based on user role

--- src/Components/Checklist/CheckListForm.jsx
+++ src/Components/Checklist/CheckListForm.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 
-export default function CheckListForm ({addGoal, exitForm}) {
-    const [goal, setGoal] = useState({
+export default function CheckListForm ({addItem, exitForm, isStudent}) {
+    const [item, setItem] = useState({
         text: "",
         category: "",
         completed: false,
@@ -9,12 +9,12 @@ export default function CheckListForm ({addGoal, exitForm}) {
     })
     const handleChange = (e) => {
         const {name, value} = e.target
-        setGoal(prev => ({...prev, [name]: value}))
+        setItem(prev => ({...prev, [name]: value}))
     }
     const handleSubmit = (e) => {
         e.preventDefault()
-        addGoal(goal)
-        setGoal({
+        addItem(item)
+        setItem({
             text: "",
             category: "",
             completed: false,
@@ -25,31 +25,41 @@ export default function CheckListForm ({addGoal, exitForm}) {
     return (
         <form className="checklist-form" onSubmit={handleSubmit}>
             <button onClick={exitForm}>Exit</button>
-            <label>
-                What Is Your Goal?
-            </label>
-            <textarea
-                name="text"
-                value={goal.text}
-                required
-                onChange={(e)=>handleChange(e)}
-            />
-            <label>
-                Which Position Does Your Goal Relate To?
-            </label>
-            <select
-                name="category"
-                value={goal.category}
-                required
-                onChange={(e)=>handleChange(e)}
-            >
-                <option value="">Select A Position</option>
-                <option value="guard">Guard</option>
-                <option value="mount">Mount</option>
-                <option value="sidecontrol">Side Control</option>
-                <option value="backcontrol">Back Control</option>
-                <option value="turtle">Turtle</option>
-            </select>
+            {isStudent ? 
+            <> 
+                <label>
+                    What Is Your Goal?
+                </label>
+                <textarea
+                    name="text"
+                    value={item.text}
+                    required
+                    onChange={(e)=>handleChange(e)}
+                />
+                <label>
+                    Which Position Does Your Goal Relate To?
+                </label>
+                <select
+                    name="category"
+                    value={item.category}
+                    required
+                    onChange={(e)=>handleChange(e)}
+                >
+                    <option value="">Select A Position</option>
+                    <option value="guard">Guard</option>
+                    <option value="mount">Mount</option>
+                    <option value="sidecontrol">Side Control</option>
+                    <option value="backcontrol">Back Control</option>
+                    <option value="turtle">Turtle</option>
+                </select>
+            </> : <>
+                <label>Add A To Do:</label>
+                <textarea 
+                    value={item.text}
+                    name="text"
+                    onChange={(e) => handleChange(e)}
+                />
+            </>}
             <button type="submit">Create</button>
 
         </form>
