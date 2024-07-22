d3fe1a25987f5f7786ac19b44905dbc733ea810e
choijake0207 <jake.ch0712@gmail.com>
2024-07-22 15:11:09 -0700

    wrap content in PageLayout

--- src/Pages/Members.jsx
+++ src/Pages/Members.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import MemberList from '../Components/Members/MemberList'
+import PageLayout from '../Layouts/PageLayout'
 import "../Styles/Members.css"
 
 export default function Members() {
   return (
-    <div className="members">
-        <MemberList/>
-    </div>
+    <PageLayout pageType={"Members Directory"}>
+        <div className="members">
+            <MemberList/>
+        </div>
+    </PageLayout>
   )
 }
