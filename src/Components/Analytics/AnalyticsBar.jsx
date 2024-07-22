ed3a500026df5169a43223b2342a19f33c07d841
choijake0207 <jake.ch0712@gmail.com>
2024-07-22 11:28:25 -0700

    rename

---
+++ src/Components/Analytics/AnalyticsBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../../Styles/Analytics.css"
+
+export default function AnalyticsBar() {
+  return (
+    <div className="analytics-bar">AnalyticsWidget</div>
+  )
+}
