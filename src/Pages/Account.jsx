9618404fd836244e3499faa7c1d7d5f8951c4ca9
choijake0207 <jake.ch0712@gmail.com>
2024-07-23 10:14:31 -0700

    add basic to account page w nav bar

--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import PageLayout from "../Layouts/PageLayout"
 
-export default function Account() {
+export default function Account () {
+
+
   return (
-    <div>Account</div>
+    <PageLayout pageType={"Account"}>
+      <div className="account">
+        <nav className="account-nav-bar">
+          <button>Profile</button>
+          <button>Membership</button>
+        </nav>
+  
+      </div>
+
+    </PageLayout>
   )
+
 }
