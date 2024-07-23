96c5bf3742b51be2a5ff7081e92213dd8084874c
choijake0207 <jake.ch0712@gmail.com>
2024-07-23 10:40:05 -0700

    add nav button logic to conditionally render account components

--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PageLayout from "../Layouts/PageLayout"
 import "../Styles/Account.css"
 
 export default function Account () {
 
+  const [activeComponent, setActiveComponent] = useState("profile")
 
   return (
     <PageLayout pageType={"Account"}>
       <div className="account">
         <nav className="account-nav-bar">
-          <button>Profile</button>
-          <button>Membership</button>
+          <button 
+            onClick={() => setActiveComponent("profile")}
+            className={activeComponent === "profile" ? "active" : null}>
+            Profile
+          </button>
+          <button 
+            onClick={() => setActiveComponent("membership")}
+            className={activeComponent === "membership" ? "active" : null}>
+            Membership
+          </button>
         </nav>
   
       </div>
