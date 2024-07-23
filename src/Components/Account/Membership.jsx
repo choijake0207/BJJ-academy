22491c2996a39374c14e51bf4980fe27f3b0282c
choijake0207 <jake.ch0712@gmail.com>
2024-07-23 14:14:20 -0700

    create membership component

---
+++ src/Components/Account/Membership.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MembershipPlans from './MembershipPlans'
+
+export default function Membership() {
+  return (
+    <div className="membership">
+      <MembershipPlans/>
+    </div>
+  )
+}
