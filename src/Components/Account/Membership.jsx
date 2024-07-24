621c8740b57a5310a175d249459eb022a566540e
choijake0207 <jake.ch0712@gmail.com>
2024-07-24 11:53:07 -0700

    import Statements component

--- src/Components/Account/Membership.jsx
+++ src/Components/Account/Membership.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import MembershipPlans from './MembershipPlans'
 import Payment from './Payment'
+import Statements from './Statements'
 export default function Membership() {
   return (
     <div className="membership">
       <MembershipPlans/>
-      <Payment/>
+      <Payment/> 
+      <Statements/>
     </div>
   )
 } 
