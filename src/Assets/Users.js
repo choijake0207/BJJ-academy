e29acb6d516f2674be71eb54c3605dbbd90b5294
choijake0207 <jake.ch0712@gmail.com>
2024-07-24 10:11:49 -0700

    add payment property to users
    add full user props to all dummy accounts

--- src/Assets/Users.js
+++ src/Assets/Users.js
@@ -68,7 +68,14 @@ export const Users = [
         membership: {
             type: "Standard",
             start: "2021-7-12"
-        }
+        },
+        payment: [
+            {
+                id: 0,
+                bank: "MeBank",
+                number: "1234-5678-9101-1121"
+            }
+        ]
     },
     {
         id: 1,
@@ -78,7 +85,6 @@ export const Users = [
         password: "janedoe",
         email: "janedoe@email.com",
         phone: "000-000-0000",
-        emergency: "111-111-1111",
         todos: [],
         teaching: [Classes[0], Classes[1]],
         role: "Coach"
@@ -87,6 +93,11 @@ export const Users = [
         id: 2,
         firstName: "Gordon",
         lastName: "Ryan",
+        username: "gordonryan",
+        password: "gordonryan",
+        email: "gordonryan@email.com",
+        phone: "222-222-2222",
+        todos: [],
         teaching: [Classes[3]],
         role: "Coach"
     },
@@ -94,12 +105,24 @@ export const Users = [
         id: 3,
         firstName: "Nicky",
         lastName: "Ryan",
+        username: "nickyryan",
+        password: "nickyryan",
+        email: "nickyryan@email.com",
+        phone: "333-333-3333",
+        todos: [],
         attending: [Classes[3], Classes[4]],
         role: "Student",
         membership: {
             type: "Premium",
             start: "2017-1-11"
-        }
+        },
+        payment: [
+            {
+                id: 0,
+                bank: "MasterBank",
+                number: "0123-4567-8910-1112"
+            }
+        ]
     }
 ]
 
