b68297f8498ea41eb24e8f524162f4a09e332569
choijake0207 <jake.ch0712@gmail.com>
2024-07-24 11:52:45 -0700

    create statements/billings section
    import billingsUtil and wrap in useEffect
    
    render list of charges

---
+++ src/Components/Account/Statements.jsx
@@ -0,0 +1,35 @@
+import React, {useState, useEffect, useContext} from 'react'
+import { BillingsUtil } from '../../Utility/Billings'
+import { AuthContext } from '../../Context/AuthContext'
+
+
+export default function Statements() {
+    const [statements, setStatements] = useState([])
+    const {authUser} = useContext(AuthContext)
+    const profileUser = authUser
+    useEffect(() => {
+        const billings = BillingsUtil(profileUser)
+        setStatements(billings)
+    }, [])
+
+    console.log(statements)
+
+
+  return (
+    <section className="statements">
+        <h3>Statements</h3>
+        <div className="statements-container">
+            {statements.map((statement, index) => {
+                return (
+                    <div className="statement" key={index}>
+                        <p>{statement.period}</p>
+                        <p>{statement.details}</p>
+                        <p>{statement.cost}</p>
+                    </div>
+                )
+            })}
+
+        </div>
+    </section>
+  )
+}
