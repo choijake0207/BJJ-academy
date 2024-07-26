f2e5b9c2393b3dcca6f149ae67e3bb1354495dfe
choijake0207 <jake.ch0712@gmail.com>
2024-07-26 10:13:17 -0700

    create membership model file

---
+++ server/models/Membership.js
@@ -0,0 +1,14 @@
+export const Memberships = [
+    {
+        id: 0,
+        type: "Standard",
+        month: 100,
+        year: 1200
+    },
+    {
+        id: 1,
+        type: "Premium",
+        month: 150,
+        year: 1800
+    }
+]
