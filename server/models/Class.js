1b190ff130ae2e96c65f19cdd4cf86daa8b0e840
choijake0207 <jake.ch0712@gmail.com>
2024-07-26 10:12:40 -0700

    create class model file

---
+++ server/models/Class.js
@@ -0,0 +1,38 @@
+export const Classes = [
+    {
+        id: 101,
+        name: "NoGi Fundamentals",
+        teacher: "Jane Doe",
+        students: [{id: 0}],
+        start: "04:00pm",
+        end: "06:00pm",
+        day: "Monday"
+    },
+    {
+        id: 102,
+        name: "Gi Fundamentals",
+        teacher: "Jane Doe",
+        students: [{id: 0}],
+        start: "04:00pm",
+        end: "06:00pm",
+        day: "Wednesday"
+    },
+    {
+        id: 103,
+        name: "NoGi Advanced",
+        teacher: "Gordon Ryan",
+        students: [{id: 3}],
+        start: "7:00pm",
+        end: "9:00pn",
+        day: "Tuesday"
+    }, 
+    {
+        id: 104,
+        name: "Open Mat",
+        teacher: "",
+        students: [{id: 3}],
+        start: "11:00am",
+        end: "1:00pm",
+        day: "Sunday"
+    }
+]
