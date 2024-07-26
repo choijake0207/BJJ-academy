ad99381437c6eb567e3704e7a1be5ff238921cb8
choijake0207 <jake.ch0712@gmail.com>
2024-07-26 11:56:33 -0700

    restructure user model to group properties

--- client/src/Models/User.js
+++ client/src/Models/User.js
@@ -1,35 +1,50 @@
 import {Classes} from "../Models/Class"
 
 export const Users = [
-
     {
         id: 0,
-        firstName: "John",
-        lastName: "Doe",
+        username: "johndoe",
+        password: "johndoe",
+        role: "Student",
+        personal: {
+            firstName: "John",
+            lastName: "Doe",
+            phone: "000-000-0000",
+            email: "johndoe@email.com",
+            emergency: "911"
+        },
         membership: {
             type: "Standard",
-            start: "2022-07-12"
+            start: "2022-08-25",
+            payment: [
+                {
+                    id: 0,
+                    bank: "MeBank",
+                    number: "0001-0002-0003-0004",
+                    status: "active"
+                }
+            ]
         },
-        phone: "000-000-0000",
-        email: "johndoe@email.com",
-        payment: {
-            id: 0,
-            bank: "MeBank",
-            number: "0001-0002-0003-0004",
-            name: "John Doe",
-        },
-        emergency: "911",
-        role: "Student",
-        classes: [Classes[0], Classes[2]]
+        activity: {
+            todos: [],
+            classes: [Classes[0], Classes[2]]
+        }, 
     },
     {
         id: 1,
-        firstName: "Jane",
-        lastName: "Doe",
-        phone: "111-111-1111",
-        email: "janedoe@bjj.com",
-        emergency: "911",
+        username: "janedoe",
+        password: "janedoe",
         role: "Coach",
-        classes: [Classes[0], Classes[1], Classes[2]]
+        personal: {
+            firstName: "Jane",
+            lastName: "Doe",
+            phone: "111-111-1111",
+            email: "janedoe@bjj.com",
+            emergency: "911"
+        },
+        activity: {
+            todos: [],
+            classes: [Classes[0], Classes[1], Classes[2]]
+        },
     }
 ]
